ec40bead43069a2dd7438538c13e42f9cd6ee28c
ausprouty <bob.prouty@powertochange.org.au>
2020-02-19 12:12:54 +1100

    public

---
+++ public/templates/works.js
@@ -0,0 +1,11 @@
+CKEDITOR.addTemplates("default", {
+	imagesPath:  CKEDITOR.getUrl("/templates/images/"),
+	templates: [{
+		title: "Share",
+		image: "share.png",
+		description: "Image and Title",
+		html: '\x3cdiv class\x3d"lesson"\x3e\x3cimg class\x3d"lesson-icon" src\x3d"/images/sharing-life.png" /\x3e\x3cdiv class\x3d"lesson-subtitle"\x3eSHARE\x3c/div\x3e\x3c/div\x3e'
+	},
+	
+	]
+});
