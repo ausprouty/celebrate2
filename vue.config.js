f30f4701d81f84c00d1327d9121883c9b67069b4
ausprouty <bob.prouty@powertochange.org.au>
2020-02-19 12:12:07 +1100

    root

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+//const path = require("path");
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/' : '/'
+}
